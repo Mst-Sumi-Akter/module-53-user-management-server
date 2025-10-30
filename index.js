2f5592ac7a199df24d3271aeb27be6a3c6554fa6
Sumi Akter <sumiwdcse@gmail.com>
2025-10-30 13:27:09 +0600

    practice module 53 part 8

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
 
 app.get ('/',(req, res)=>{
     res.send('Hallo!')
@@ -20,7 +21,31 @@ const users = [
 app.get('/users',(req,res)=>{
     res.send(users);
 })
+// data pathate post babohar kora hoy
+app.post('/users',(req, res)=>{
+    console.log('post method called', req.body);//same name dite hbe
 
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+    users.push(newUser)
+    res.send(newUser);
+})         
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+
+
+
+/**
+ * have to send request object to the server
+ * 1. mention method: post
+ * 2. mention header: about json data in the property of content-type: application/json
+ * 3. body: JSON.stringify(newUser)
+ * 
+ * --------------
+ * on the server side use json as middleware
+ * app.use(express.json())
+ * 
+*/
